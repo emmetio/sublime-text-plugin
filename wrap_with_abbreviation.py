e75ff6963cafad75fafe0415d8746a7a9632ab5f
Sergey Chikuyonok <serge.che@gmail.com>
2019-11-20 23:49:27 +0300

    Simplified Wrap With Abbreviation action

--- wrap_with_abbreviation.py
+++ wrap_with_abbreviation.py
@@ -2,7 +2,6 @@ import re
 import sublime
 import sublime_plugin
 from . import emmet_sublime as emmet
-from . import syntax
 from . import preview
 from . import utils
 
@@ -17,18 +16,14 @@ class EmmetWrapWithAbbreviation(sublime_plugin.TextCommand):
             utils.replace_with_snippet(self.view, edit, self.region, snippet)
             last_abbreviation = wrap_abbreviation
 
-    def get_range(self):
-        sel = self.view.sel()[0]
-        opt = syntax.info(self.view, sel.begin(), 'html')
-        region = find_context_tag(self.view, sel.begin(), opt) if sel.empty() else sel
-        return region
 
     def input(self, *args, **kwargs):
         # pylint: disable=attribute-defined-outside-init
         sel = self.view.sel()[0]
-        self.region = self.get_range()
+
+        self.options = emmet.get_options(self.view, sel.begin(), True)
+        self.region = get_wrap_region(self.view, sel, self.options)
         self.lines = get_content(self.view, self.region, True)
-        self.options = syntax.info(self.view, sel.begin(), 'html')
         self.options['text'] = self.lines
 
         return WrapAbbreviationInputHandler(self.options)
@@ -63,29 +58,6 @@ class WrapAbbreviationInputHandler(sublime_plugin.TextInputHandler):
             return sublime.Html(popup_content(snippet))
 
 
-def find_context_tag(view: sublime.View, pt: int, syntax_info=None):
-    "Finds tag context for given location and returns its range, if found"
-    if syntax_info is None:
-        syntax_info = syntax.info(view, pt, 'html')
-    syntax_name = syntax_info.get('syntax')
-
-    if syntax.is_html(syntax_name):
-        ctx = emmet.get_tag_context(view, pt, syntax.is_xml(syntax_name))
-        if ctx:
-            # Check how given point relates to matched tag:
-            # if it's in either open or close tag, we should wrap tag itself,
-            # otherwise we should wrap its contents
-            open_tag = ctx.get('open')
-            close_tag = ctx.get('close')
-
-            if in_range(open_tag, pt) or (close_tag and in_range(close_tag, pt)):
-                return sublime.Region(open_tag.begin(), close_tag and close_tag.end() or open_tag.end())
-
-            if close_tag:
-                r = sublime.Region(open_tag.end(), close_tag.begin())
-                return utils.narrow_to_non_space(view, r)
-
-
 def in_range(region: sublime.Region, pt: int):
     return region.begin() < pt < region.end()
 
@@ -116,3 +88,26 @@ def popup_content(content: str):
         <div>%s</div>
     </body>
     """ % content
+
+
+def get_wrap_region(view: sublime.View, sel: sublime.Region, options: dict) -> sublime.Region:
+    "Returns region to wrap with abbreviation"
+    if sel.empty() and options.get('context'):
+        # If there’s no selection than user wants to wrap current tag container
+        ctx = options['context']
+        pt = sel.begin()
+
+        # Check how given point relates to matched tag:
+        # if it's in either open or close tag, we should wrap tag itself,
+        # otherwise we should wrap its contents
+        open_tag = ctx.get('open')
+        close_tag = ctx.get('close')
+
+        if in_range(open_tag, pt) or (close_tag and in_range(close_tag, pt)):
+            return sublime.Region(open_tag.begin(), close_tag and close_tag.end() or open_tag.end())
+
+        if close_tag:
+            r = sublime.Region(open_tag.end(), close_tag.begin())
+            return utils.narrow_to_non_space(view, r)
+
+    return sel
